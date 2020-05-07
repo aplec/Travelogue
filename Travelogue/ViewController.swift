735263db51a9b172290e9be33ddc26fdc7918fa6
Ante Plecas <anteplecas@Antes-MBP.attlocal.net>
2020-05-07 02:50:06 -0500

    First commit

---
+++ Travelogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travelogue
+//
+//  Created by Ante Plecas on 5/7/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
